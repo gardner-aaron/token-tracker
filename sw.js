ce37b941687a6a1e8f2bbf9d41fc36ad1a641beb
eli-gardner <43885148+eli-gardner@users.noreply.github.com>
2026-01-24 18:59:44 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'token-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './Clue.jpg',
+  './Food.jpg',
+  './Treasure.jpg',
+  './Dino_Clue.jpg',
+  './Dino_Food.jpg',
+  './Dino_Treasure.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
